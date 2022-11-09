98f55d756af1b7c7eaf3a49ffbeedc521c20dd9f
NatalyLu <natulya.lukashina@bk.ru>
2022-11-09 13:38:50 +0300

    new error block

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -3,23 +3,26 @@ import s from './profile.module.css';
 import img from '../../images/lions.jpg'
 import ProfileInfo from './profile-info/profile-info'
 import Loader from '../loader/loader';
+import ErrorBlock from '../error/error-block';
 
 const Profile = (props) => {
   return (
     <section className={s.profile}>
-      {!props.profile
-        ? <Loader />
-        : <>
-            <img
-              className={s.profile__img}
-              src={img}
-              alt="Lions"
-              width="256"
-              height="171"
-            ></img>
-            <ProfileInfo styles={s} profile={props.profile} />
-            <PostsContainer />
-          </>
+      {props.isFetchingProfile
+        ? <Loader /> 
+        : props.profile 
+            ? <>
+                <img
+                  className={s.profile__img}
+                  src={img}
+                  alt="Lions"
+                  width="256"
+                  height="171"
+                ></img>
+                <ProfileInfo styles={s} profile={props.profile} />
+                <PostsContainer />
+              </>
+            : <ErrorBlock />
       }
     </section>
   )
