40c55de6c68befc8b6125a69f5a9569ea4e3e8e9
NatalyLu <natulya.lukashina@bk.ru>
2022-11-10 18:24:33 +0300

    created login component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import DialogsContainer from "./components/dialogs/dialogs-container";
 import HeaderContainer from "./components/header/header-container";
+import Login from "./components/login/login";
 import Navigation from './components/navigation/navigation';
 import ProfileContainer from "./components/profile/profile-container";
 import UsersContainer from "./components/users/users-container";
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/dialogs" element={<DialogsContainer />} />
           <Route path="/profile/:id?" element={<ProfileContainer />} />
           <Route path="/users" element={<UsersContainer />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<div>404 Page not found</div>} />
         </Routes>
       </main>
