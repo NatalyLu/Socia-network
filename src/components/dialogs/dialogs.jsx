a7587d873400413dc84526d423a1b705e6b415cb
NatalyLu <natulya.lukashina@bk.ru>
2022-11-25 22:01:50 +0300

    added TextareaForm into dialogs and posts components

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -1,40 +1,28 @@
-import {useForm} from "react-hook-form";
 import s from "./dialogs.module.css";
 import Dialog from "./dialog/dialog";
 import Messages from "./messages/messages";
-import { DialogSymbols } from "../../const";
+import { DialogSymbols, Errors } from "../../const";
+import TextareaForm from "../common/textarea-form/textarea-form";
 
-const AddMessageForm = (props) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({});
-
+const Dialogs = (props) => {
   let addMessageHandler = (data) => {
     props.sendMessage(data.newMessage);
   }
-
-  return(
-    <form onSubmit={handleSubmit(addMessageHandler)} className={s.messages__new}>
-      <textarea
-        className={`textarea ${errors.newMessage && "error"}`}
-        {...register("newMessage", {required: true, minLength: DialogSymbols.MIN, maxLength:DialogSymbols.MAX })}
-        placeholder="Enter your message"></textarea>
-        {errors.newMessage && <p className="text-error">Message's length must be more then {DialogSymbols.MIN} symbols, but less then {DialogSymbols.MAX}.</p>}
-      <button type="submit" className="submit" disabled={!isValid}>Send</button>
-    </form>
-  )
-}
-
-const Dialogs = (props) => {
+  const registerObj = {required: true, minLength: DialogSymbols.MIN, maxLength:DialogSymbols.MAX };
 
   return (
     <div className={s.dialogs}>
       <Dialog users={props.dialogs.users} />
       <div className={`${s.dialogs__messages} ${s.messages}`}>
         <Messages messages={props.dialogs.messages} />
-        <AddMessageForm sendMessage={props.sendMessage}/>
+        <TextareaForm
+          submitHandler={addMessageHandler}
+          fieldName="newMessage"
+          registerObj={registerObj}
+          placeholder="Enter your message"
+          errorText={Errors.DIALOG_TEXTAREA_ERROR}
+          buttonText="Send"
+        />
       </div>
     </div>
   );
--- src/components/profile/posts/posts.jsx
+++ src/components/profile/posts/posts.jsx
@@ -1,39 +1,27 @@
 import React from "react";
-import {useForm} from "react-hook-form";
 import Post from "./post/post";
 import s from "./posts.module.css";
 import avatar from "../../../images/avatar-1.jpg";
-import { TextareaSymbols } from "../../../const";
+import { Errors, TextareaSymbols } from "../../../const";
+import TextareaForm from "../../common/textarea-form/textarea-form";
 
-const AddPostForm = (props) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isValid },
-  } = useForm({
-    mode: "onChange"
-  });
-
+const Posts = (props) => {
   let addPostHandler = (data) => {
     props.addPost(data.newPost);
   }
 
-  return(
-    <form onSubmit={handleSubmit(addPostHandler)} className={s.posts__new}>
-      <textarea
-        className={`textarea ${errors.newPost && "error"}`}
-        {...register("newPost", {required: true, minLength: TextareaSymbols.MIN, maxLength: TextareaSymbols.MAX })}
-      />
-      {errors.newPost && <p className="text-error">Post's length must be more then {TextareaSymbols.MIN} symbols and less then {TextareaSymbols.MAX}.</p>}
-      <button type="submit" className="submit" disabled={!isValid}>Add posts</button>
-    </form>
-  )
-}
+  const registerObj = {required: true, minLength: TextareaSymbols.MIN, maxLength: TextareaSymbols.MAX };
 
-const Posts = (props) => {
   return(
     <section className={s.posts}>
-      <AddPostForm addPost={props.addPost} />
+      <TextareaForm
+        mode="onChange"
+        submitHandler={addPostHandler}
+        fieldName="newPost"
+        registerObj={registerObj}
+        errorText={Errors.POST_TEXTAREA_ERROR}
+        buttonText="Add posts"
+      />
       <ul className={s.posts__list}>
         {props.posts.map((item) => <Post key={item.id} link={avatar}>{item.text}</Post>)}
       </ul>
